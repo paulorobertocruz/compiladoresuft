8d5be3b32ee6146c474480b1f06c342e78ab45df
Paulo Roberto Cruz <paulo.cruz9@gmail.com>
2018-03-15 00:04:44 -0300

    implementa função que converte expressao com concatenações implicitas e uso da contra barra para escapar operadores

--- posfixa.py
+++ posfixa.py
@@ -1,58 +1,218 @@
+OPERADOR = 0
+OPERANDO = 1
+OPERADORES = {"+", ".", "*", "(", ")"}
+PRECEDENCIA = {"+":0, ".":1, "*":2, "(":99, ")":99}
+
+
+class Opera(object):
+    """
+    Operador ou Operando
+    """
+
+    str = None
+    tipo = None
+
+    def __init__(self, str, tipo=OPERADOR):
+        self.str = str
+        self.tipo = tipo
+
+    def __str__(self):
+        return "({0} {1})".format(self.str, self.operando_ou_operador())
+
+    def __repr__(self):
+        return "({0} {1})".format(self.str, self.operando_ou_operador())
+
+    def operando_ou_operador(self):
+        if self.tipo == OPERADOR:
+            return "operador"
+        else:
+            return "operando"
+
+    def igual(self, caractere):
+        return self.str == caractere
+
+
+class Expressao(object):
+    lista = []
+
+    def add(self, opera):
+        self.lista.append(opera)
+
+    def pop(self):
+        if len(self.lista) > 0:
+            op = self.lista[0]
+
+        if len(self.lista) > 1:
+            self.lista = self.lista[1:]
+        else:
+            self.lista = []
+
+        return op
+
+    def __len__(self):
+        return len(self.lista)
+
+    def __str__(self):
+        rr = ""
+        for i in self.lista:
+            rr += i.str
+        return rr
+
+
+def contra_barra(caractere):
+    if caractere in OPERADORES or caractere == "\\":
+        return True
+    return False
+
+
+def expressao_lista_opera(expressao):
+    expressao = expressao.replace(" ", "")
+    operadores = {"+", ".", "*", "(", ")"}
+    lista_opera = list()
+    express = Expressao()
+
+    while len(expressao) > 0:
+        ex_op = None
+
+        expressao_1 = None
+        if expressao[0] == "\\":
+            if len(expressao) > 1:
+                expressao_1 = expressao[1]
+            else:
+                print("erro, não tem caractere depois da barra")
+                return None
+
+            if len(expressao) > 2:
+                expressao = expressao[:1] + expressao[2:]
+            else:
+                expressao = expressao[0]
+
+        if len(expressao) > 1:
+
+            # a.a
+            if expressao[0] not in operadores and expressao[1] not in operadores:
+                ex_op = Opera(".", tipo=OPERADOR)
+
+            # a.(
+            elif expressao[0] not in operadores and expressao[1] == "(":
+                ex_op = Opera(".", tipo=OPERADOR)
+
+            # ).a
+            elif expressao[0] == ")" and expressao[1] not in operadores:
+                ex_op = Opera(".", tipo=OPERADOR)
+
+            # ).(
+            elif expressao[0] == ")" and expressao[1] == "(":
+                ex_op = Opera(".", tipo=OPERADOR)
+
+            # *.a
+            elif expressao[0] == "*" and expressao[1] not in operadores:
+                ex_op = Opera(".", tipo=OPERADOR)
+
+            # *.(
+            elif expressao[0] == "*" and expressao[1] == "(":
+                ex_op = Opera(".", tipo=OPERADOR)
+
+
+        if expressao[0] in operadores:
+            op = Opera(expressao[0], tipo=OPERADOR)
+            lista_opera.append(op)
+            express.add(op)
+            expressao = expressao[1:]
+
+        elif expressao[0] == "\\":
+            #pronblema de comparação
+            op = Opera( "{0}{1}".format(expressao[0], expressao_1), tipo=OPERANDO)
+            lista_opera.append(op)
+            express.add(op)
+            expressao = expressao[1:]
+
+        else:
+            op = Opera(expressao[0], tipo=OPERANDO)
+            lista_opera.append(op)
+            express.add(op)
+            expressao = expressao[1:]
+
+        if ex_op is not None:
+            lista_opera.append(ex_op)
+            express.add(ex_op)
+
+    return express
 
 def infixa_posfixa(entrada):
 
-    #remove espaços
-    entrada = entrada.replace(" ", "")
-    print(entrada)
+    expressao = expressao_lista_opera(entrada)
     posfixa = ""
-    operadores = {"+", ".", "*", "(", ")"}
-    precedencia = {"+":0, ".":1, "*":2, "(":99, ")":99}
+
     pilha = list()
-    entrada_1 = None
-    entrada_2 = None
 
-    while len(entrada) > 0:
-        entrada_1 = entrada_2
-        entrada_2 = None
+    while len(expressao) > 0:
 
-        atual = entrada[0]
-        entrada = entrada[1:]
+        atual = expressao.pop()
 
-        if atual in operadores:
-            entrada_2 = "operador"
+        if atual.tipo == OPERADOR:
 
-            if atual == "(":
+            if atual.igual("("):
                 pilha.append(atual)
-
-            elif atual == ")":
+            elif atual.igual(")"):
                 if len(pilha) > 0:
-                    while len(pilha) > 0 and pilha[-1] != "(":
-                        posfixa += pilha.pop()
-                    if len(pilha) > 0 and pilha[-1] == "(":
+                    while len(pilha) > 0 and not pilha[-1].igual("("):
+                        posfixa += pilha.pop().str
+                    if len(pilha) > 0 and pilha[-1].igual("("):
                         pilha.pop()
                     else:
                         print("erro, parenteses")
             else:
-                while len(pilha) > 0 and precedencia[atual] <= precedencia[pilha[-1]] and pilha[-1] not in {"(", ")"}:
-                    posfixa += pilha.pop()
+                while len(pilha) > 0 and PRECEDENCIA[atual.str] <= PRECEDENCIA[pilha[-1].str] and pilha[-1].str not in {"(", ")"}:
+                    posfixa += pilha.pop().str
                 pilha.append(atual)
         else:
-            posfixa += atual
-            entrada_2 = "operando"
-
-            if entrada_1 == entrada_2 and entrada_1 == "operando":
-                while len(pilha) > 0 and precedencia["."] <= precedencia[pilha[-1]] and pilha[-1] not in {"(", ")"}:
-                    posfixa += pilha.pop()
-                pilha.append(".")
-        print(pilha)
-        print(posfixa)
+            posfixa += atual.str
 
     while len(pilha) > 0:
-        posfixa += pilha.pop()
+        posfixa += pilha.pop().str
 
-    print(pilha)
-    print(posfixa)
-
+    return posfixa
 
 
-infixa_posfixa("(a + b) * c")
+aaa = "\\++b(ab)"
+r = infixa_posfixa(aaa)
+
+print(aaa)
+print(r)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
